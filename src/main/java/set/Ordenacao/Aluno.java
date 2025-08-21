408d0e000b069b04b6705ea92df7ba86acb8ad15
Andrew <andrewfreiria28@gmail.com>
2025-08-21 01:53:07 -0300

    Resolvendo exercicio Ordenacao
    Signed-off-by: Andrew <andrewfreiria28@gmail.com>

---
+++ src/main/java/set/Ordenacao/Aluno.java
@@ -0,0 +1,74 @@
+package main.java.set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno> {
+    //atributos
+    private String nome;
+    private long matricula;
+    private double nota;
+
+    public Aluno(String nome, long matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    @Override
+    public int compareTo(Aluno aluno) {
+        return nome.compareToIgnoreCase(aluno.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return matricula == aluno.matricula;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matricula);
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", nota=" + nota +
+                '}';
+    }
+}
+
+class ComparatorPorNota implements Comparator<Aluno> {
+    @Override
+    public int compare(Aluno a1, Aluno a2) {
+        return Double.compare(a1.getNota(), a2.getNota());
+    }
+}
---
+++ src/main/java/set/Ordenacao/Produto.java
@@ -0,0 +1,85 @@
+package main.java.set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto> {
+    //atributos
+    private String nome;
+    private long cod;
+    private double preco;
+    private int quantidade;
+
+    public Produto(long cod, String nome, double preco, int quantidade) {
+        this.cod = cod;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto produto) {
+        return nome.compareToIgnoreCase(produto.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public void setCod(long cod) {
+        this.cod = cod;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return cod == produto.cod;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cod);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", cod=" + cod +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Produto> {
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+}
